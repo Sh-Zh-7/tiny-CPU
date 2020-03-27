afa14049e55ed6af926fb6a90a4016309d783e62
Sh-Zh-7 <2431297348@qq.com>
2020-03-27 14:03:23 +0800

    Update pc

--- source/PC.v
+++ source/PC.v
@@ -1,5 +1,6 @@
-module PC( clk, rst, NPC, PC );
+`include "../include/ctrl_encode_def.v"
 
+module PC(clk, rst, NPC, PC);
   input              clk;
   input              rst;
   input       [31:0] NPC;
@@ -10,7 +11,51 @@ module PC( clk, rst, NPC, PC );
       PC <= 32'h0000_0000;
       //PC <= 32'h0000_3000;
     else
-      PC <= NPC;
-      
+      PC <= NPC;   
+endmodule
+
+module PCSrc(Jump, Branch, Zero, NPCOp);
+    input[1:0] Jump;
+    input[1:0] Branch;
+    input Zero;
+    output reg[1:0] NPCOp;
+    always @(*) 
+    begin
+        if (Jump == 2'd1) 
+            NPCOp = `NPC_JUMP_IMM;
+        else if (Jump == 2'd2)
+            NPCOp = `NPC_JUMP_REG;
+        else if (Branch == 2'd1 && Zero || Branch == 2'd2 && !Zero) 
+            NPCOp = `NPC_BRANCH;
+        else
+            NPCOp = `NPC_PLUS4;
+    end
+endmodule 
+
+module NPC(PC, NPCOp, IMM, addr, NPC);  
+   input  [31:0] PC;        
+   input  [1:0]  NPCOp;     
+   input  [31:0] IMM;       
+   input  [31:0] addr;
+   output reg [31:0] NPC;   
+   
+   wire [31:0] PCPLUS4;
+   
+   assign PCPLUS4 = PC + 4; 
+   
+   always @(*) begin
+      case (NPCOp)
+		   `NPC_PLUS4:  NPC = PCPLUS4;
+		   // Branch专属
+		   // PC+4+sign_extend(offset||0^2)
+		   `NPC_BRANCH: NPC = PCPLUS4 + {{14{IMM[15]}}, IMM[15:0], 2'b00};
+		   // Jump到立即数
+		   // PC的高四位，立即数左移两位拼接
+		   `NPC_JUMP_IMM:   NPC = {PCPLUS4[31:28], IMM[25:0], 2'b00};
+		   // Jump到寄存器中的地址
+		   `NPC_JUMP_REG:   NPC = addr;
+         default:     NPC = PCPLUS4;
+      endcase
+   end
 endmodule
 
